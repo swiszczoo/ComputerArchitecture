a724269be656591f7993ef7fa81a61f3ec44b598
Łukasz Świszcz <luk.swiszcz@gmail.com>
2022-05-09 00:17:33 +0200

    Task2: Add a test, use exactly one register less

--- task2.asm
+++ task2.asm
@@ -14,6 +14,10 @@ nines1: .byte 0xff 0xff 0xff 0xff 0xff 0xff 0xff
 nines2: .byte 0x00 0x00 0x83 0x63 0x19 0x24 0x2f
 nines3: .byte 0x00 0x0f 0xff 0xff 0xff 0xff 0xff 0xff
 
+space1: .byte 0x6f
+space2: .byte 0x8f
+space3: .space 2
+
 .text
 main:
 	la 	$a0,	number1
@@ -43,6 +47,12 @@ main:
 		jal	bcdPrint
 		subiu	$s0,	$s0,	1
 		bgtz	$s0,	main_loop
+		
+	la 	$a0,	space1
+	la	$a1,	space2
+	la	$a2,	space3
+	jal	bcdAdd # test4: 6 + 8 = 14 (result longer than either of the operands)
+	jal	bcdPrint
 	
 	li	$v0,	10
 	syscall
@@ -59,13 +69,13 @@ bcdAdd:
 	# t4 - length of the first term in digits
 	# t5 - length of the second term in digits
 	# t6 - calculated length of the result (+1 to store 0xf)
-	# t7 - are we R/W-ing more significant half-byte? (bit mask, bit 2^0 - first term, bit 2^1 - second term, bit 2^2 - result)
+	# t7 - are we R/W-ing more significant half-byte? / carry flag
+	#      (bit mask, 2^0 - first term, 2^1 - second term, 2^2 - result, 2^3 - carry flag)
 	# t8 - count of processed digits
-	# t9 - carry flag
-	# v0 - return address
+	# t9 - return address
 	
 	move	$t2,	$a0
-	move	$v0,	$ra
+	move	$t9,	$ra
 	li	$t4,	0
 	loopLength1:			# calculate length of the first term
 		lbu	$t0,	($t2)		# load next byte to $t0
@@ -95,7 +105,7 @@ bcdAdd:
 	endLoopLength2:
 	# firstly perform dry run addition and propagate carry flag to calculate the
 	# length of the result
-	andi	$t7,	$t5,	0x01 	# set first flag of $t7
+	andi	$t7,	$t5,	0x01 	# set first flag of $t7 + clear carry
 	sll	$t7,	$t7,	1
 	andi	$t0,	$t4,	0x01
 	or	$t7,	$t7,	$t0	# set second flag of $t7
@@ -108,7 +118,6 @@ bcdAdd:
 	addu	$t3,	$a1,	$t0	# store address of the last digit in $t3
 	li	$t6,	0		# set current result length to 0
 	li	$t8,	0		# set processed digits count to 0
-	li	$t9, 	0		# set carry to 0
 	
 	loopLength3:
 		jal 	_bcdAddStep
@@ -128,14 +137,15 @@ bcdAdd:
 		# loop conditions:
 		bge	$t2,	$a0,	loopLength3
 		bge	$t3,	$a1,	loopLength3
-		bnez	$t9,	loopLength3
+		andi	$t0,	$t7,	0x08
+		bnez	$t0,	loopLength3
 	# should we represent 0 as 0x0f or 0xf0 in result?
 	bgtz	$t6,	adding
 	li	$t6,	2	# change 2 to 1 to represent 0 as 0xf0
 	# now finally add two numbers
 	# change t8 meaning - current result address
 	adding:
-	andi	$t7,	$t6,	0x01	# set result flag of $t7
+	andi	$t7,	$t6,	0x01	# set result flag of $t7 + clear carry
 	sll	$t7,	$t7,	2
 	andi	$t0,	$t5,	0x01 	
 	sll	$t0,	$t0,	1
@@ -152,7 +162,6 @@ bcdAdd:
 	subiu	$t0,	$t5,	1
 	srl	$t0,	$t0,	1
 	addu	$t3,	$a1,	$t0	# store address of the last digit in $t3
-	li	$t9, 	0		# set carry to 0
 	
 	andi	$t0,	$t7,	0x04
 	xori	$t7,	$t7,	0x04	# flip upper/lower flag for result
@@ -191,12 +200,12 @@ bcdAdd:
 		subiu	$t6,	$t6,	1
 		bgtz	$t6,	loopAdd	# there are more digits left to add
 		
-	jr 	$v0 		# return
+	jr 	$t9 		# return
 	
 	_bcdAddStep:		# internal subroutine to prevent duplicate code, t0 contains current digit, t9 - carry
 		# first digit:
 		li	$t0,	0
-		blt	$t2,	$a0,	_bcdAddStep_addr1Underflow	# if $t2 is less than $a0, the first term was shorter and we use 0
+		blt	$t2,	$a0,	_bcdAddStep_addr1Underflow	# if $t2 is less than $a0, the first term has ended and we use 0
 		andi	$t0,	$t7,	0x01		# put upper half-byte flag in $t0
 		beqz	$t0,	_bcdAddStep_addr1LSB	# load upper half-byte:
 		lbu	$t0,	($t2)				# load current byte to $t0
@@ -208,7 +217,7 @@ bcdAdd:
 		_bcdAddStep_addr1Underflow:
 		# second digit:
 		li	$t1,	0
-		blt	$t3,	$a1	_bcdAddStep_addr2Underflow	# if $t3 is less than $a1, the second term was shorter and we use 0
+		blt	$t3,	$a1	_bcdAddStep_addr2Underflow	# if $t3 is less than $a1, the second term has ended and we use 0
 		andi	$t1,	$t7,	0x02		# put upper half-byte flag in $t1
 		beqz	$t1,	_bcdAddStep_addr2LSB	# load upper half-byte:
 		lbu	$t1,	($t3)				# load current byte to $t1
@@ -220,11 +229,13 @@ bcdAdd:
 		_bcdAddStep_addr2Underflow:
 		# add two digits and carry
 		addu	$t0,	$t0,	$t1
-		addu	$t0,	$t0,	$t9
-		li	$t9,	0			# clear carry
+		andi	$t1,	$t7,	0x08 		# extract carry flag from $t7
+		xor	$t7,	$t7,	$t1		# clear carry (x XOR x = 0)
+		srl	$t1,	$t1,	3
+		addu	$t0,	$t0,	$t1
 		blt	$t0,	10,	_bcdAddStep_noCarry
 		subiu	$t0,	$t0,	10		# apply BCD bias
-		li	$t9,	1			# set carry
+		ori	$t7,	$t7,	0x08		# set carry
 		_bcdAddStep_noCarry:
 		jr	$ra
 
